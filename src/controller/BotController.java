b8f5ae7654d0204b440f07c10424699e41bb3ff2
Eden Chambers <EdenChambers5@gmail.com>
2018-10-12 12:58:33 -0600

    I added my runner, made some lists, and am beginning my getters and setters.

---
+++ src/controller/BotController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import model.Chatbot;
+
+public class BotController
+{
+
+	public void start()
+	{
+		
+	}
+	
+	ArrayList<String> responseList = new ArrayList<String>();
+	
+}
---
+++ src/controller/BotRunner.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class BotRunner
+{
+	public static void main (String [] args)
+	{
+		BotController app = new BotController();
+		app.start ();
+				
+	}
+}
