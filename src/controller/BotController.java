fbcad2e0e1a536f598d48327f820472d2243761e
Eden Chambers <EdenChambers5@gmail.com>
2018-10-12 13:53:48 -0600

    I finished the getters and setters as well as adding a while loop requiring you to type quit. :)

--- src/controller/BotController.java
+++ src/controller/BotController.java
@@ -9,9 +9,15 @@ public class BotController
 
 	public void start()
 	{
-		
+		String userInput = "Something.";
+		while(!userInput.equalsIgnoreCase ("quit"))
+		{
+			userInput = JOptionPane.showInputDialog (null, "If you want me to stop, type 'quit'.");
+		}
 	}
 	
 	ArrayList<String> responseList = new ArrayList<String>();
+	
+	ArrayList<String> spookyList = new ArrayList<String>(); 
 	
 }
--- src/model/Chatbot.java
+++ src/model/Chatbot.java
@@ -1,6 +1,80 @@
 package model;
 
-public class Chatbot
+public class Chatbot 
 {
-
+	private String askName;
+	private String catResponse;
+	private String hello; 
+	private String howAreYou;
+	private String niceToMeetYou;
+	private String userName;
+	private String good;
+	private String bad;
+	private String whatWasThat;
+	
+	public Chatbot()
+	{
+		this.askName = "What is your name?";
+		this.catResponse = "I like cats. :)";
+		this.hello = "Hello.";
+		this.niceToMeetYou = "It is nice to meet you."; 
+		this.howAreYou = "How are you doing this fine morning?";
+		this.good = "That's good. :) ";
+		this.bad = "That sucks. :( "; 
+	}
+	
+	public Chatbot(String askName, String catResponse,String whatWasThat, String hello, String howAreYou, String niceToMeetYou, String userName, String good, String bad)
+	{
+		this.askName = askName; 
+		this.catResponse = catResponse; 
+		this.hello = hello; 
+		this.niceToMeetYou = niceToMeetYou; 
+		this.howAreYou = howAreYou;
+		this.userName = userName;
+		this.good = good;
+		this.bad = bad;
+		this.whatWasThat = whatWasThat;
+	}
+	
+	
+	public String getAskName()
+	{
+		return askName;
+	}
+	public String getCatResponse()
+	{
+		return catResponse;
+	}
+	public String getHello()
+	{
+		return hello;
+	}
+	public String getHowAreYou()
+	{
+		return howAreYou;
+	}
+	public String getNiceToMeetYou()
+	{
+		return niceToMeetYou;
+	}
+	public String getUserName()
+	{
+		return userName;
+	}
+	public String getGood()
+	{
+		return good;
+	}
+	public String getBad()
+	{
+		return bad;
+	}
+	
+	public void setUserName(String userName)
+	{
+		this.userName = userName;
+	}
+	
+	
+	
 }
