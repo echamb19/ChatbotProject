c4f2eb53a9b9e7de32f6ee60e0219fa5738b6201
Eden Chambers <EdenChambers5@gmail.com>
2018-12-05 12:07:51 -0700

    I set up the Panel and the ScrollPane in the ChatPanel. I also added the SpringLayout to the Panel.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,7 @@
 package chat.view;
 
 import javax.swing.*;
+import java.awt.Dimension; 
 import chat.controller.ChatController; 
 import java.awt.event.ActionListener; 
 import java.awt.event.ActionEvent; 
@@ -11,6 +12,7 @@ public class ChatPanel extends JPanel
 {
 	private ChatController chatController; 
 	//private JLabel responseLabel; 
+	private SpringLayout appLayout; 
 	private JButton chatButton;
 	private JButton checkerButton; 
 	private JButton loadButton; 
@@ -24,16 +26,20 @@ public class ChatPanel extends JPanel
 		super(); 
 		
 		this.chatController = chatController; 
+		appLayout = new SpringLayout(); 
+		
 		this.chatButton = new JButton("To Chat"); 
-		this.checkerButton = new JButton("Checker");
+		this.checkerButton = new JButton("Check Text");
 		this.loadButton = new JButton("Load"); 
 		this.saveButton = new JButton("Save"); 
-		this.chatField = new JTextField(); 
-		this.chatArea = new JTextArea(); 
+		
+		this.chatField = new JTextField("Talk to the bot here.", 50); //columns not x value
+		this.chatArea = new JTextArea("Chat Area", 20, 50); 
 		this.chatPane = new JScrollPane(); 
 		
 		//this.responseLabel = new JLabel("Hello, I am your friendly neighborhood Chatbot."); 
 		
+		setupScrollPane(); 
 		setupPanel(); 
 		setupLayout(); 
 		setupListeners(); 
@@ -41,7 +47,15 @@ public class ChatPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(800, 600)); 
+		this.setBackground(Color.BLUE); 
+		this.add(chatPane); 
+		this.add(chatButton); 
+		this.add(saveButton); 
+		this.add(loadButton); 
+		this.add(checkerButton); 
+		this.add(chatField); 
 	}
 	
 	private void setupListeners()
@@ -54,4 +68,14 @@ public class ChatPanel extends JPanel
 		
 	}
 
+	private void setupScrollPane()
+	{
+		chatArea.setEditable(false); //Can't type into it
+		chatArea.setLineWrap(true); 
+		chatArea.setWrapStyleWord(true); 
+		
+		chatPane.setViewportView(chatArea); 
+		chatPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); 
+		chatPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED); 
+	}
 }
