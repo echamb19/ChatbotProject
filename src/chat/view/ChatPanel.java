993685a28b9ac4f3f5d08a4588f45eb9d6a2f257
Eden Chambers <EdenChambers5@gmail.com>
2018-12-05 11:43:54 -0700

    I added the necessary data memebers and got rid of the unnecessary ones.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -10,16 +10,29 @@ import java.awt.Font;
 public class ChatPanel extends JPanel
 {
 	private ChatController chatController; 
-	private JLabel responseLabel; 
-	private JButton responseButton;
+	//private JLabel responseLabel; 
+	private JButton chatButton;
+	private JButton checkerButton; 
+	private JButton loadButton; 
+	private JButton saveButton; 
+	private JTextField chatField; 
+	private JTextArea chatArea; 
+	private JScrollPane chatPane; 
 	
 	public ChatPanel(ChatController chatController)
 	{
 		super(); 
 		
 		this.chatController = chatController; 
+		this.chatButton = new JButton("To Chat"); 
+		this.checkerButton = new JButton("Checker");
+		this.loadButton = new JButton("Load"); 
+		this.saveButton = new JButton("Save"); 
+		this.chatField = new JTextField(); 
+		this.chatArea = new JTextArea(); 
+		this.chatPane = new JScrollPane(); 
 		
-		this.responseLabel = new JLabel("Hello, I am your friendly neighborhood Chatbot."); 
+		//this.responseLabel = new JLabel("Hello, I am your friendly neighborhood Chatbot."); 
 		
 		setupPanel(); 
 		setupLayout(); 
